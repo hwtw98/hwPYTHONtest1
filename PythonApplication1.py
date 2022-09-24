acf28e97ccc88a62104fd119828782fa4660617e
Howard Wu <hwtw98@gmail.com>
2022-09-24 11:20:57 +0800

    Add files via upload

---
+++ PythonApplication1.py
@@ -0,0 +1,22 @@
+from tkinter import END, OptionMenu
+from tracemalloc import start
+
+start
+c = ("My Twitter Is @thehwtw My Instagram Is @thehwtw")
+b = ("A.Yes")
+d = ("A.No")
+corid33 =("Is E Bigger then A?")
+dodddddd =("join my discord!: hwli.tk/discord ")
+ciddi =("Press O When Ask For Press. . .")
+a = 1
+e = 2
+print(corid33)
+if e > a:
+  print(b)
+if e < a:
+    print(d)
+print(c)
+print(dodddddd)
+print("hw " * 10)
+print('*' * 30)
+print(ciddi)
